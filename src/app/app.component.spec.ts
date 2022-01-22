ca9b6e67a807ede41115269fe4e312fd3c892998
jeo1421 <shajithmon21@gmail.com>
2022-01-22 09:47:56 +0530

    full

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'salesteamapp'`, () => {
+  it(`should have as title 'sales'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('salesteamapp');
+    expect(app.title).toEqual('sales');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('salesteamapp app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('sales app is running!');
   });
 });
